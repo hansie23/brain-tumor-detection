3d6140ebe36845c4b0c100406182a312f34268ad
Hansie <63493237+hansie23@users.noreply.github.com>
2024-06-15 05:54:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import numpy as np
 input_shape = (112, 112, 3)
 weights_path = r"unet_weights.h5"
 
-
 # Load the model
 @st.cache_resource
 def model(input_shape, weights_path):
@@ -32,25 +31,37 @@ def predict(image, model):
 
 
 # Streamlit UI
-st.title("Brain Tumor Segmentation")
-
-uploaded_image = st.file_uploader("Upload an MRI image", type=["tif"])
-
-if uploaded_image is not None:
-    image = Image.open(uploaded_image)
+def main():
+    st.title("Brain Tumor Segmentation")
     
-    col1, col2 = st.columns(2)
-    
-    with col1:
-        st.image(image, caption='Uploaded MRI Image', use_column_width=True)
+    # Define page options
+    page_options = ['Home', 'Model Interpretability']
 
-    if st.button('Predict'):
-        prediction = predict(uploaded_image, model)
-        
-        # Post-process prediction to display
-        prediction_image = np.squeeze(prediction)  # Remove batch dimension
-        prediction_image = (prediction_image * 255).astype(np.uint8)  # Scale back to [0, 255]
-        prediction_image = Image.fromarray(prediction_image)
-        
-        with col2:
-            st.image(prediction_image, caption='Predicted Brain Tumor', use_column_width=True)
+    # Display navigation
+    st.sidebar.title('Navigation')
+    page_selection = st.sidebar.radio('Go to', page_options)
+    
+    # Display selected page
+    if page_selection == 'Home':
+        uploaded_image = st.file_uploader("Upload an MRI image", type=["tif"])
+        if uploaded_image is not None:
+            image = Image.open(uploaded_image)
+            col1, col2 = st.columns(2)
+            with col1:
+                st.image(image, caption='Uploaded MRI Image', use_column_width=True)
+            if st.button('Predict'):
+                prediction = predict(uploaded_image, model)
+                
+                # Post-process prediction to display
+                prediction_image = np.squeeze(prediction)  # Remove batch dimension
+                prediction_image = (prediction_image * 255).astype(np.uint8)  # Scale back to [0, 255]
+                prediction_image = Image.fromarray(prediction_image)
+                
+                with col2:
+                    st.image(prediction_image, caption='Predicted Brain Tumor', use_column_width=True)
+
+    elif page_selection == 'Model Interpretability':
+        st.image('interpretability.png', use_column_width=True)
+
+if __name__ == '__main__':
+    main()
